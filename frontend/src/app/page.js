abb1cd42c076adfe28e0202ddfdb97f8ea0c8f30
RebecaZaia <rebeca.zaia@estudante.ifms.edu.br>
2026-04-24 15:09:22 -0400

    modified:   frontend/src/app/page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -17,21 +17,3 @@ export default function Home() {
     </div>
   );
 }
-
-/*export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Home
-        </h1>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
-          <a href="/login">Login</a>
-        </button>
-        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700">
-          <a href="/cadastrar">Cadastrar</a>
-        </button>
-      </main>
-    </div>
-  );
-}*/
